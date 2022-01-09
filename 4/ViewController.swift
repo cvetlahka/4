ef574892ca92b4f5fb625c79d1201df8aa4f385a
Cvetlahka <Cvetlahka@yandex.ru>
2022-01-09 10:06:31 +0300

    projekt

---
+++ 4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4
+//
+//  Created by Светлана Сафонова on 08.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
